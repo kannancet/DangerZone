fd26d93dca67c4e5ca356081afaab79a1d0f9d58
qbadmin <qbadmin@qbadmin-desktop.(none)>
2013-11-28 15:22:14 +0530

    source code version1.0

---
+++ airmelive/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Airmelive::Application.config.secret_key_base = '546f19b6b9371c64e7d165c653c6f88903a285b2433c99ae748760a40052f3a69d20385bc5ad9db7a8f40fc5d1619163bfe5f69937ccd065d99d7c3bbc7af3f4'
---
+++ airmelive/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Airmelive::Application.config.session_store :cookie_store, key: '_airmelive_session'
